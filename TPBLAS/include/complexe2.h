add17986b9812c48983e7853296ae8d8f0594f69
Roman <romanregouin@live.fr>
2020-02-19 14:54:11 +0100

    Div complexe

--- TPBLAS/include/complexe2.h
+++ TPBLAS/include/complexe2.h
@@ -9,8 +9,7 @@ typedef struct {
   double imaginary ;
 } complexe_double_t ;
 
-inline complexe_float_t add_complexe_float (const complexe_float_t c1, const complexe_float_t c2)
-{
+inline complexe_float_t add_complexe_float (const complexe_float_t c1, const complexe_float_t c2){
   complexe_float_t r ;
 
   r.real = c1.real + c2.real ;
@@ -19,8 +18,7 @@ inline complexe_float_t add_complexe_float (const complexe_float_t c1, const com
   return r ;
 }
 
-inline complexe_double_t add_complexe_double (const complexe_double_t c1, const complexe_double_t c2)
-{
+inline complexe_double_t add_complexe_double (const complexe_double_t c1, const complexe_double_t c2){
   complexe_double_t r ;
 
   r.real = c1.real + c2.real ;
@@ -30,28 +28,35 @@ inline complexe_double_t add_complexe_double (const complexe_double_t c1, const 
 }
 
 
-/*
-inline complexe_float_t mult_complexe_float (const complexe_float_t c1, const complexe_float_t c2)
-{
-
+inline complexe_float_t mult_complexe_float (const complexe_float_t c1, const complexe_float_t c2){
+  complexe_float_t r ;
+  r.imaginary = c1.real * c2.imaginary + c1.imaginary * c2.real;
+  r.real = c1.real * c2.real - (c1.imaginary * c2.imaginary);
+  return r ;
 }
 
-inline complexe_double_t mult_complexe_double (const complexe_double_t c1, const complexe_double_t c2)
-{
+inline complexe_double_t mult_complexe_double (const complexe_double_t c1, const complexe_double_t c2){
+  complexe_double_t r ;
 
+  r.imaginary = c1.real * c2.imaginary + c1.imaginary * c2.real;
+  r.real = c1.real * c2.real - (c1.imaginary * c2.imaginary);
+  return r ;
 }
 
 
-inline complexe_float_t div_complexe_float (const complexe_float_t c1, const complexe_float_t c2)
-{
-
+inline complexe_float_t div_complexe_float (const complexe_float_t c1, const complexe_float_t c2){
+  complexe_float_t r;
+  r.imaginary = ( c1.imaginary * c2.real - c1.real * c2.imaginary ) / ( ( c2.real * c2.real ) + ( c2.imaginary * c2.imaginary ) );
+  r.real = ( c1.real * c2.real + c1.imaginary * c2.imaginary ) / ( ( c2.real * c2.real ) + ( c2.imaginary * c2.imaginary ) );
+  return r;
 }
 
-inline complexe_double_t div_complexe_double (const complexe_double_t c1, const complexe_double_t c2)
-{
-
+inline complexe_double_t div_complexe_double (const complexe_double_t c1, const complexe_double_t c2){
+  complexe_double_t r;
+  r.imaginary = ( c1.imaginary * c2.real - c1.real * c2.imaginary ) / ( ( c2.real * c2.real ) + ( c2.imaginary * c2.imaginary ) );
+  r.real = ( c1.real * c2.real + c1.imaginary * c2.imaginary ) / ( ( c2.real * c2.real ) + ( c2.imaginary * c2.imaginary ) );
+  return r;
 }
-*/
 
 
 
--- TPBLAS/src/complexe.c
+++ TPBLAS/src/complexe.c
@@ -1,7 +1,6 @@
 #include "complexe.h"
 
-complexe_float_t add_complexe_float (const complexe_float_t c1, const complexe_float_t c2)
-{
+complexe_float_t add_complexe_float (const complexe_float_t c1, const complexe_float_t c2){
   complexe_float_t r ;
 
   r.real = c1.real + c2.real ;
@@ -10,8 +9,7 @@ complexe_float_t add_complexe_float (const complexe_float_t c1, const complexe_f
   return r ;
 }
 
-complexe_double_t add_complexe_double (const complexe_double_t c1, const complexe_double_t c2)
-{
+complexe_double_t add_complexe_double (const complexe_double_t c1, const complexe_double_t c2){
   complexe_double_t r ;
 
   r.real = c1.real * c2.real - c1.imaginary * c2.imaginary;
@@ -20,22 +18,35 @@ complexe_double_t add_complexe_double (const complexe_double_t c1, const complex
   return r ;
 }
 
-complexe_float_t mult_complexe_float (const complexe_float_t c1, const complexe_float_t c2)
-{
+complexe_float_t mult_complexe_float (const complexe_float_t c1, const complexe_float_t c2){
   complexe_float_t r ;
   r.imaginary = c1.real * c2.imaginary + c1.imaginary * c2.real;
   r.real = c1.real * c2.real - (c1.imaginary * c2.imaginary);
   return r ;
 }
 
-complexe_double_t mult_complexe_double (const complexe_double_t c1, const complexe_double_t c2)
-  {
+complexe_double_t mult_complexe_double (const complexe_double_t c1, const complexe_double_t c2){
   complexe_double_t r ;
 
   r.imaginary = c1.real * c2.imaginary + c1.imaginary * c2.real;
   r.real = c1.real * c2.real - (c1.imaginary * c2.imaginary);
   return r ;
 }
+
+complexe_float_t div_complexe_float (const complexe_float_t c1, const complexe_float_t c2){
+  complexe_float_t r;
+  r.imaginary = ( c1.imaginary * c2.real - c1.real * c2.imaginary ) / ( ( c2.real * c2.real ) + ( c2.imaginary * c2.imaginary ) );
+  r.real = ( c1.real * c2.real + c1.imaginary * c2.imaginary ) / ( ( c2.real * c2.real ) + ( c2.imaginary * c2.imaginary ) );
+  return r;
+}
+
+complexe_double_t div_complexe_double (const complexe_double_t c1, const complexe_double_t c2){
+  complexe_double_t r;
+  r.imaginary = ( c1.imaginary * c2.real - c1.real * c2.imaginary ) / ( ( c2.real * c2.real ) + ( c2.imaginary * c2.imaginary ) );
+  r.real = ( c1.real * c2.real + c1.imaginary * c2.imaginary ) / ( ( c2.real * c2.real ) + ( c2.imaginary * c2.imaginary ) );
+  return r;
+}
+
   
 
 
