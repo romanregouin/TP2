5e8b7fe5a33a8d1f77937dd83dc787dc74d57b85
Andrieux <andrieuxliam@gmail.com>
2020-02-19 14:33:09 +0100

    complexe.c

--- TPBLAS/src/complexe.c
+++ TPBLAS/src/complexe.c
@@ -23,7 +23,7 @@ complexe_double_t add_complexe_double (const complexe_double_t c1, const complex
 complexe_float_t mult_complexe_float (const complexe_float_t c1, const complexe_float_t c2)
 {
   complexe_float_t r ;
-  r.imaginary = 0.0;
+  r.imaginary = c1.real * c2.imaginary + c1.imaginary * c2.real;
   r.real = c1.real * c2.real - (c1.imaginary * c2.imaginary);
   return r ;
 }
@@ -32,9 +32,8 @@ complexe_double_t mult_complexe_double (const complexe_double_t c1, const comple
   {
   complexe_double_t r ;
 
-  r.real = 0.0 ;
-  r.imaginary = 0.0 ;
-  
+  r.imaginary = c1.real * c2.imaginary + c1.imaginary * c2.real;
+  r.real = c1.real * c2.real - (c1.imaginary * c2.imaginary);
   return r ;
 }
   
